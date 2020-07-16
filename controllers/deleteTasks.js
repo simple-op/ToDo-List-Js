e626844518dad58e0d471f45588464f38a91f419
unknown <you@example.com>
2020-07-16 23:09:25 +0530

    done

--- controllers/deleteTasks.js
+++ controllers/deleteTasks.js
@@ -3,9 +3,8 @@ const task=require("../models/taskList")
 
 
 
-
 let deleteTasks=function(req,res){
-    
+if(req.body.check)   {
 
  task.findByIdAndDelete({_id:req.body.check},function(err,object){
       
@@ -15,7 +14,7 @@ let deleteTasks=function(req,res){
     for(let i=0;i<req.body.check.length;i++){
     task.findByIdAndDelete({_id:req.body.check[i]},function(err,object){
       
-    })}
+    })}}
     res.redirect("/");
     console.log(req.body);
 
